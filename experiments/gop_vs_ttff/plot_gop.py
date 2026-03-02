48e3201d28a7216a5b54e5c3044e6b87258a80a7
zhaoyuxuansoft <459763664@qq.com>
2026-03-02 19:01:56 +0000

    upgrade plot_gop.py

--- experiments/gop_vs_ttff/plot_gop.py
+++ experiments/gop_vs_ttff/plot_gop.py
@@ -1,13 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("resolution_ttff.csv")
+# 读取数据
+df = pd.read_csv("gop_ttff.csv")
 
+# 计算每个 GOP 的 mean 和 std
+grouped = df.groupby("gop")["ttff_ms"]
+mean = grouped.mean()
+std = grouped.std()
+
+# 画图
 plt.figure()
-plt.errorbar(df["resolution"], df["mean"], yerr=df["std"], marker='o')
-plt.xlabel("Resolution")
+plt.errorbar(mean.index, mean.values, yerr=std.values, marker='o')
+
+plt.xlabel("GOP Size")
 plt.ylabel("Mean TTFF (ms)")
-plt.title("Resolution vs TTFF")
+plt.title("GOP vs TTFF")
 plt.grid(True)
-plt.savefig("resolution_vs_ttff.png")
+
+plt.savefig("gop_vs_ttff.png", dpi=300)
 plt.show()
